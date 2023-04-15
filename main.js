1ded23caa27b22cf2dc24477021301eb20277758
alphajuliet <andrew@alphajuliet.com>
2023-04-15 14:43:38 +1000

    Added EP04. Renaming. Update style.

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ const Cyjet = (() => {
   const Info = {
     title: "cyjet",
     author: "AndrewJ",
-    version: "0.1",
-    date: "2023-03-25",
+    version: "0.2",
+    date: "2023-04-14",
     info: "Cyjet home",
     appendTitleTo: (tagName) => {
       $(tagName).append($(`<span class="title">cчjεt</span>`));
@@ -52,7 +52,7 @@ const Cyjet = (() => {
         { site: "Apple Music", href: "https://geo.music.apple.com/album/dust-craft-ep/1653157543" },
         { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BLHY5LPQ?ref=dm_ff_linkfire" },
       ],
-      trackList: []
+      trackList: ["Angel Deep", "Occultation", "Distant Touch", "Metal City Dub"]
     },
     {
       artist: "Cyjet",
@@ -62,13 +62,13 @@ const Cyjet = (() => {
       releaseText: "november 2022",
       links: [
         { site: "All sites", href: "https://artists.landr.com/cargo-cult" },
-        { site: "Bandcamp", href: "http://bandcamp.com/cyjet/cargo-cult" },
+        { site: "Bandcamp", href: "https://bandcamp.com/cyjet/cargo-cult" },
         { site: "Spotify", href: "https://open.spotify.com/album/65f26Ij2tWTVeGfBn67zQi" },
         { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_m8w3wWYdBSNnLPh_wKI_Ziu0D1IvjKnPM" },
         { site: "Apple Music", href: "https://geo.music.apple.com/album/cargo-cult-ep/1658040968" },
         { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BP2RV7LG?ref=dm_ff_linkfire" },
       ],
-      trackList: []
+      trackList: ["Gamboge", "Londeth", "Shades of January", "Do You Love?"]
     },
     {
       artist: "Cyjet",
@@ -78,13 +78,29 @@ const Cyjet = (() => {
       releaseText: "march 2023",
       links: [
         { site: "All sites", href: "https://artists.landr.com/cyjet-scan-lines" },
-        { site: "Bandcamp", href: "http://bandcamp.com/cyjet/scan-lines" },
+        { site: "Bandcamp", href: "https://bandcamp.com/cyjet/scan-lines" },
         { site: "Spotify", href: "https://open.spotify.com/album/6mW2yAqITqCMA8Oo60gIbk" },
         { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_knYV1jeY4jkXNBiQ01en3p9kmGVn5Arro" },
         { site: "Apple Music", href: "https://geo.music.apple.com/album/scan-lines-ep/1675167080" },
         { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BX8ZGDDH?ref=dm_ff_linkfire" },
       ],
-      trackList: []
+      trackList: ["Servolex", "Cube", "Coma Trip", "Snow at Easter"]
+    },
+    {
+      artist: "Cyjet",
+      title: "neon ride",
+      cover_image: "images/ep04 neon-ride 1024x1024.jpg",
+      releaseDate: "2023-04",
+      releaseText: "april 2023",
+      links: [
+        { site: "All sites", href: "https://artists.landr.com/cyjet-neon-ride" },
+        { site: "Bandcamp", href: "https://bandcamp.com/cyjet/neon-ride" },
+        { site: "Spotify", href: "https://open.spotify.com/album/5viirWr8G3z1tRK8bmpoqh" },
+        { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_mNVd3VT72flKorj0J_xcVKIJEoWlx6w1c" },
+        { site: "Apple Music", href: "" },
+        { site: "Amazon Music", href: "https://music.amazon.com/albums/B0C2D5R8GM?ref=dm_ff_linkfire" },
+      ],
+      trackList: ["No-one Else Around", "Sexkontakt", "Fluorescence", "As Colours Edge"]
     }
   ];
 
@@ -100,27 +116,22 @@ const Cyjet = (() => {
   });
 
   const renderRelease = R.curry((target, release) => {
-    const container2 = $(`<div class="release-info"></div>`);
+    const release_info = $(`<div class="release-info"></div>`);
+
     const title = $(`<div class="title">${ release.title }</div>`);
     const releaseText = $(`<div>${ release.releaseText }</div>`);
-    const info = $(container2).append_(title)
-                              .append_(releaseText);
+    const info = $(release_info).append_(title).append_(releaseText);
     const info2 = renderLinks(info, release);
 
     const container1 = $(`<div class="release"></div>`);
     const image = $(`<img class="image" src="${release.cover_image}"/>`)
 
-    const rel = $(container1).append_(image)
-                             .append_(info2);
+    const rel = $(container1).append_(image).append_(info2);
     return $(target).append_(rel);
   });
 
-  // -------------------
-  // Render all public tracks by a nominated key. Also a reducing function.
-  // renderByPropTo :: jQuery -> Object -> jQuery
-
   const renderReleasesTo = R.curry((target, corpus) => {
-    const container1 = $(`<div class="releases"></div>`);
+    const releases = $(`<div class="releases"></div>`);
     return R.reduce(renderRelease, target, corpus);
   });
 
