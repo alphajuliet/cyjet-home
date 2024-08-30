e6acf562318d87815a9f8b2ccc6dd5a09d25ab72
alphajuliet <andrew@alphajuliet.com>
2024-08-30 15:28:07 +1000

    Add YouTube link for ep10

--- releases.js
+++ releases.js
@@ -9,16 +9,37 @@ const MyReleases = [
     releaseDate: "2024-08",
     releaseText: "august 2024",
     links: [
-      { site: "All sites", href: "https://artists.landr.com/cyjet-assemblage" },
-      { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/assemblage" },
-      { site: "Spotify", href: "https://open.spotify.com/album/7J5zn4vqJLQK2SQ236Gj2S" },
-      { site: "YouTube Music", href: "//" },
-      { site: "Apple Music", href: "https://music.apple.com/us/album/assemblage-ep/1763489361" },
-      { site: "iTunes", href: "https://music.apple.com/us/album/assemblage-ep/1763489361" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0DDHBQRLB?ref=dm_ff_linkfire" },
+      {
+        site: "All sites",
+        href: "https://artists.landr.com/cyjet-assemblage",
+      },
+      {
+        site: "Bandcamp",
+        href: "https://cyjet.bandcamp.com/album/assemblage",
+      },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/7J5zn4vqJLQK2SQ236Gj2S",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_lqQ31VG-wZ-jfVMnuBPUhQwUEM8koM3Ss",
+      },
+      {
+        site: "Apple Music",
+        href: "https://music.apple.com/us/album/assemblage-ep/1763489361",
+      },
+      {
+        site: "iTunes",
+        href: "https://music.apple.com/us/album/assemblage-ep/1763489361",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0DDHBQRLB?ref=dm_ff_linkfire",
+      },
       { site: "Deezer", href: "https://www.deezer.com/album/631190451" },
     ],
-    trackList: ["Indicator", "Burning Tree", "Orenea", "Ethera"]
+    trackList: ["Indicator", "Burning Tree", "Orenea", "Ethera"],
   },
   {
     artist: "Cyjet",
@@ -28,15 +49,36 @@ const MyReleases = [
     releaseDate: "2024-05",
     releaseText: "may 2024",
     links: [
-      { site: "All sites", href: "https://artists.landr.com/cyjet-tidal-forms" },
-      { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/tidal-forms" },
-      { site: "Spotify", href: "https://open.spotify.com/album/4uqstauoaZi7vaXKyfVLVn" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_lqnAauCz1Vq2qdWtfBfD3zIqPcs1dZOw4" },
-      { site: "Apple Music", href: "https://music.apple.com/jp/album/tidal-forms-ep/1748353310?l=en-US" },
-      { site: "iTunes", href: "https://music.apple.com/jp/album/tidal-forms-ep/1748353310?l=en-US" },
-      { site: "Amazon Music", href: "https://music.amazon.com.au/albums/B0D57S6H1D" },
+      {
+        site: "All sites",
+        href: "https://artists.landr.com/cyjet-tidal-forms",
+      },
+      {
+        site: "Bandcamp",
+        href: "https://cyjet.bandcamp.com/album/tidal-forms",
+      },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/4uqstauoaZi7vaXKyfVLVn",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_lqnAauCz1Vq2qdWtfBfD3zIqPcs1dZOw4",
+      },
+      {
+        site: "Apple Music",
+        href: "https://music.apple.com/jp/album/tidal-forms-ep/1748353310?l=en-US",
+      },
+      {
+        site: "iTunes",
+        href: "https://music.apple.com/jp/album/tidal-forms-ep/1748353310?l=en-US",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com.au/albums/B0D57S6H1D",
+      },
     ],
-    trackList: ["Mash", "Memory", "Darius", "Where to begin?"]
+    trackList: ["Mash", "Memory", "Darius", "Where to begin?"],
   },
   {
     artist: "Cyjet",
@@ -48,13 +90,28 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cyjet-dead-north" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/dead-north" },
-      { site: "Spotify", href: "https://open.spotify.com/album/5zgWTkoQSpnEymwVsY5iMi" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_mtrgODvzHhqMzH7an9HA_xtHqGbU86hlU" },
-      { site: "Apple Music", href: "https://geo.music.apple.com/album/dead-north-ep/1733094270" },
-      { site: "iTunes", href: "https://geo.music.apple.com/album/dead-north-ep/1733094270" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0CWJMFQ2W?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/5zgWTkoQSpnEymwVsY5iMi",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_mtrgODvzHhqMzH7an9HA_xtHqGbU86hlU",
+      },
+      {
+        site: "Apple Music",
+        href: "https://geo.music.apple.com/album/dead-north-ep/1733094270",
+      },
+      {
+        site: "iTunes",
+        href: "https://geo.music.apple.com/album/dead-north-ep/1733094270",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0CWJMFQ2W?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["Overheard", "Triphex", "Layer", "Arodove"]
+    trackList: ["Overheard", "Triphex", "Layer", "Arodove"],
   },
   {
     artist: "Cyjet",
@@ -64,15 +121,36 @@ const MyReleases = [
     releaseDate: "2023-12",
     releaseText: "december 2023",
     links: [
-      { site: "All sites", href: "https://artists.landr.com/cyjet-switch-break" },
-      { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/switch-break" },
-      { site: "Spotify", href: "https://open.spotify.com/album/0kVMqplY6WADOs853XmvLG" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_mJmWfriVWpcZ-jNiRpssP0geqsMmnNVLY" },
-      { site: "Apple Music", href: "https://music.apple.com/us/album/switch-break-ep/1720999334?app=music" },
-      { site: "iTunes", href: "https://music.apple.com/us/album/switch-break-ep/1720999334" },
-      { site: "Amazon Music", href: "https://music.amazon.com.au/albums/B0CQ2QNTG4" },
+      {
+        site: "All sites",
+        href: "https://artists.landr.com/cyjet-switch-break",
+      },
+      {
+        site: "Bandcamp",
+        href: "https://cyjet.bandcamp.com/album/switch-break",
+      },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/0kVMqplY6WADOs853XmvLG",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_mJmWfriVWpcZ-jNiRpssP0geqsMmnNVLY",
+      },
+      {
+        site: "Apple Music",
+        href: "https://music.apple.com/us/album/switch-break-ep/1720999334?app=music",
+      },
+      {
+        site: "iTunes",
+        href: "https://music.apple.com/us/album/switch-break-ep/1720999334",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com.au/albums/B0CQ2QNTG4",
+      },
     ],
-    trackList: ["Desert Shift", "Olivine", "Diana Flats, 1932", "Zone Block"]
+    trackList: ["Desert Shift", "Olivine", "Diana Flats, 1932", "Zone Block"],
   },
   {
     artist: "Cyjet",
@@ -82,15 +160,36 @@ const MyReleases = [
     releaseDate: "2023-08",
     releaseText: "august 2023",
     links: [
-      { site: "All sites", href: "https://artists.landr.com/cyjet-light-chaser" },
-      { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/light-chaser" },
-      { site: "Spotify", href: "https://open.spotify.com/album/1IyaT7G1eO1sXqpvMdgVEW" },
-      { site: "YouTube Music", href: "https://geo.music.apple.com/album/light-chaser-ep/1704877693" },
-      { site: "Apple Music", href: "https://music.apple.com/us/album/light-chaser-ep/1704877693" },
-      { site: "iTunes", href: "https:geo.music.apple.com/album/light-chaser-ep/1704877693" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0CGVTNVSR" },
+      {
+        site: "All sites",
+        href: "https://artists.landr.com/cyjet-light-chaser",
+      },
+      {
+        site: "Bandcamp",
+        href: "https://cyjet.bandcamp.com/album/light-chaser",
+      },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/1IyaT7G1eO1sXqpvMdgVEW",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://geo.music.apple.com/album/light-chaser-ep/1704877693",
+      },
+      {
+        site: "Apple Music",
+        href: "https://music.apple.com/us/album/light-chaser-ep/1704877693",
+      },
+      {
+        site: "iTunes",
+        href: "https:geo.music.apple.com/album/light-chaser-ep/1704877693",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0CGVTNVSR",
+      },
     ],
-    trackList: ["Versoix", "Leftstep", "Wanda in Christmas", "Dovetail"]
+    trackList: ["Versoix", "Leftstep", "Wanda in Christmas", "Dovetail"],
   },
   {
     artist: "Cyjet",
@@ -102,12 +201,24 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cyjet-dark-lands" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/dark-lands" },
-      { site: "Spotify", href: "https://open.spotify.com/album/2megbRNMYGnJD9IzB4TqXq" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_k5a_JR2Yc5inwTCCSKGljF5JzI8mosWe0" },
-      { site: "Apple Music", href: "https://geo.music.apple.com/album/dark-lands-ep/1693368775?app=music" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0C8PQS17N?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/2megbRNMYGnJD9IzB4TqXq",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_k5a_JR2Yc5inwTCCSKGljF5JzI8mosWe0",
+      },
+      {
+        site: "Apple Music",
+        href: "https://geo.music.apple.com/album/dark-lands-ep/1693368775?app=music",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0C8PQS17N?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["Zzent", "Threadhead", "Elastic", "Problem Child"]
+    trackList: ["Zzent", "Threadhead", "Elastic", "Problem Child"],
   },
   {
     artist: "Cyjet",
@@ -119,12 +230,29 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cyjet-neon-ride" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/neon-ride" },
-      { site: "Spotify", href: "https://open.spotify.com/album/5EjvQurRjUUJLJWcSCSlhp" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_mNVd3VT72flKorj0J_xcVKIJEoWlx6w1c" },
-      { site: "Apple Music", href: "https://music.apple.com/au/album/neon-ride-ep/1682262897" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0C2D5R8GM?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/5EjvQurRjUUJLJWcSCSlhp",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_mNVd3VT72flKorj0J_xcVKIJEoWlx6w1c",
+      },
+      {
+        site: "Apple Music",
+        href: "https://music.apple.com/au/album/neon-ride-ep/1682262897",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0C2D5R8GM?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["No-one Else Around", "Sexkontakt", "Fluorescence", "As Colours Edge"]
+    trackList: [
+      "No-one Else Around",
+      "Sexkontakt",
+      "Fluorescence",
+      "As Colours Edge",
+    ],
   },
   {
     artist: "Cyjet",
@@ -136,12 +264,24 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cyjet-scan-lines" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/scan-lines" },
-      { site: "Spotify", href: "https://open.spotify.com/album/6mW2yAqITqCMA8Oo60gIbk" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_knYV1jeY4jkXNBiQ01en3p9kmGVn5Arro" },
-      { site: "Apple Music", href: "https://geo.music.apple.com/album/scan-lines-ep/1675167080" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BX8ZGDDH?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/6mW2yAqITqCMA8Oo60gIbk",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_knYV1jeY4jkXNBiQ01en3p9kmGVn5Arro",
+      },
+      {
+        site: "Apple Music",
+        href: "https://geo.music.apple.com/album/scan-lines-ep/1675167080",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0BX8ZGDDH?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["Servolex", "Cube", "Coma Trip", "Snow at Easter"]
+    trackList: ["Servolex", "Cube", "Coma Trip", "Snow at Easter"],
   },
   {
     artist: "Cyjet",
@@ -153,12 +293,24 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cargo-cult" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/cargo-cult" },
-      { site: "Spotify", href: "https://open.spotify.com/album/65f26Ij2tWTVeGfBn67zQi" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_m8w3wWYdBSNnLPh_wKI_Ziu0D1IvjKnPM" },
-      { site: "Apple Music", href: "https://geo.music.apple.com/album/cargo-cult-ep/1658040968" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BP2RV7LG?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/65f26Ij2tWTVeGfBn67zQi",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_m8w3wWYdBSNnLPh_wKI_Ziu0D1IvjKnPM",
+      },
+      {
+        site: "Apple Music",
+        href: "https://geo.music.apple.com/album/cargo-cult-ep/1658040968",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0BP2RV7LG?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["Gamboge", "Londeth", "Shades of January", "Do You Love?"]
+    trackList: ["Gamboge", "Londeth", "Shades of January", "Do You Love?"],
   },
   {
     artist: "Cyjet",
@@ -170,13 +322,25 @@ const MyReleases = [
     links: [
       { site: "All sites", href: "https://artists.landr.com/cyjet-dust-craft" },
       { site: "Bandcamp", href: "https://cyjet.bandcamp.com/album/dust-craft" },
-      { site: "Spotify", href: "https://open.spotify.com/album/2zYz71mHvjHZe3SsUcf1Dr" },
-      { site: "YouTube Music", href: "https://music.youtube.com/playlist?list=OLAK5uy_mSsPea67U7Aeh7C7olJ_hqjp7dJJyjIw4" },
-      { site: "Apple Music", href: "https://geo.music.apple.com/album/dust-craft-ep/1653157543" },
-      { site: "Amazon Music", href: "https://music.amazon.com/albums/B0BLHY5LPQ?ref=dm_ff_linkfire" },
+      {
+        site: "Spotify",
+        href: "https://open.spotify.com/album/2zYz71mHvjHZe3SsUcf1Dr",
+      },
+      {
+        site: "YouTube Music",
+        href: "https://music.youtube.com/playlist?list=OLAK5uy_mSsPea67U7Aeh7C7olJ_hqjp7dJJyjIw4",
+      },
+      {
+        site: "Apple Music",
+        href: "https://geo.music.apple.com/album/dust-craft-ep/1653157543",
+      },
+      {
+        site: "Amazon Music",
+        href: "https://music.amazon.com/albums/B0BLHY5LPQ?ref=dm_ff_linkfire",
+      },
     ],
-    trackList: ["Angel Deep", "Occultation", "Distant Touch", "Metal City Dub"]
-  }
+    trackList: ["Angel Deep", "Occultation", "Distant Touch", "Metal City Dub"],
+  },
 ];
 
 // The End
